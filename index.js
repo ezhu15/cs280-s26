470b1a962aab6a13b4da3de1ebb267eb7628e007
ewar <ezhu15@jh.edu>
2026-02-01 15:04:15 -0500

    wakeuptimes

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("hi");
+const now = new Date();
+console.log(now.toLocaleString('en-US', {
+    timeStyle : "short"
+}));
+
+const fallAsleepTime = new Date(now);
+fallAsleepTime.setMinutes(
+    fallAsleepTime.getMinutes() + 14
+);
+console.log(fallAsleepTime.toLocaleString('en-US', {
+    timeStyle : "short"
+}));
+
+const wakeUpTime = new Date(fallAsleepTime);
+const wakeUpTimes = [];
+for (let i = 0; i < 6; i++){
+    wakeUpTime.setMinutes(
+        wakeUpTime.getMinutes() + 90
+    );
+    const wakeUpTimeString = wakeUpTime.toLocaleString('en-US', {
+        timeStyle : "short"
+    });
+    wakeUpTimes.push(wakeUpTimeString);
+}
+console.log(wakeUpTimes.join(", "));
+
+let x = "5" + 5;
+console.log(x);
+x = x - 5;
+console.log(x);
